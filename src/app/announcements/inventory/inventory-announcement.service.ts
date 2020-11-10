dfafaaa6e7a4af6207c3ec411031c19d65d033d9
Owen Mecham <owen.mecham@adesa.com>
2020-11-10 15:05:38 -0700

    tranlsation service updates

--- src/app/announcements/inventory/inventory-announcement.service.ts
+++ src/app/announcements/inventory/inventory-announcement.service.ts
@@ -1,43 +1,50 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable, OnDestroy } from '@angular/core';
-import { BehaviorSubject, combineLatest, Subject } from 'rxjs';
-import { take, takeUntil } from 'rxjs/operators';
-
-import { AppService } from '../../app.service';
-import { ApiUrlType, SettingsService } from '../../shared/settings.service';
+import { BehaviorSubject, combineLatest, of, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { EventDispatchService } from '../../shared/event-dispatch.service';
+import { TranslationService } from '../../shared/translation.service';
 import { AnnouncementOption } from '../announcement-option';
 import { filterAvailableAnnouncements } from '../announcements-functions';
-import { EventDispatchService } from '../../shared/event-dispatch.service';
 import { InventoryAnnouncement } from './inventory-announcement';
-import { TranslationService } from '../../shared/translation.service';
 
 @Injectable()
 export class InventoryAnnouncementService implements OnDestroy {
-  inventoryAnnouncementsList$: BehaviorSubject<InventoryAnnouncement[]> = new BehaviorSubject(null);
-  filteredAnnouncementOptions$: BehaviorSubject<AnnouncementOption[]> = new BehaviorSubject(null);
-  availableAnnouncementOptions$: BehaviorSubject<AnnouncementOption[]> = new BehaviorSubject(null);
-  private inventoryId: number;
+  sampleJson: InventoryAnnouncement[] = [
+    {
+      description: 'ABS Defective',
+      announcementId: 1,
+      announcementSequence: 1,
+      displayStatus: 'ABS Defective',
+      inventoryId: 1,
+    },
+  ];
+
+  availableJson: AnnouncementOption[] = [
+    {
+      id: 1,
+      description: 'ABS Defective',
+      isBeingAdded: false,
+    },
+  ];
+
+  inventoryAnnouncementsList$: BehaviorSubject<InventoryAnnouncement[]> = new BehaviorSubject(
+    this.sampleJson,
+  );
+  filteredAnnouncementOptions$: BehaviorSubject<AnnouncementOption[]> = new BehaviorSubject(
+    this.availableJson,
+  );
+  availableAnnouncementOptions$: BehaviorSubject<AnnouncementOption[]> = new BehaviorSubject(
+    this.availableJson,
+  );
+  // private inventoryId = 1;
   private unsubscribe$ = new Subject<void>();
 
   constructor(
-    private httpClient: HttpClient,
-    private appService: AppService,
-    private settingsService: SettingsService,
     private eventDispatchService: EventDispatchService,
     private translationService: TranslationService,
   ) {
-    this.appService.inventoryId$.pipe(takeUntil(this.unsubscribe$)).subscribe(
-      (inventoryId) => {
-        if (!inventoryId) {
-          return;
-        }
-        this.inventoryId = inventoryId;
-        this.refreshAnnouncements();
-      },
-      (error) => {
-        throw error;
-      },
-    );
+    // this.inventoryId = 1;
+    this.refreshAnnouncements();
 
     combineLatest([this.inventoryAnnouncementsList$, this.availableAnnouncementOptions$])
       .pipe(takeUntil(this.unsubscribe$))
@@ -52,48 +59,16 @@ export class InventoryAnnouncementService implements OnDestroy {
   }
 
   addInventoryAnnouncement(announcement: AnnouncementOption): void {
-    const url = this.settingsService.createUrl(
-      `vehicles/v1/${this.inventoryId}/specialAnnouncements`,
-      ApiUrlType.Inventory,
-    );
-
-    const announcementId = announcement.id;
-    this.httpClient
-      .post<InventoryAnnouncement>(url, { announcementId }, this.settingsService.createRequestOptions())
-      .pipe(take(1))
-      .subscribe(
-        (resp) => {
-          this.eventDispatchService.dispatchEvent('inventoryAnnouncementAdded', resp);
-          this.refreshAnnouncements();
-        },
-        (error) => {
-          announcement.isBeingAdded = false;
-          throw error;
-        },
-      );
+    this.eventDispatchService.dispatchEvent('inventoryAnnouncementAdded', announcement);
+    this.refreshAnnouncements();
   }
 
   deleteInventoryAnnouncement(announcementId: number, announcementSequence: number): void {
-    const url = this.settingsService.createUrl(
-      `vehicles/v1/${this.inventoryId}/specialAnnouncements/${announcementId}`,
-      ApiUrlType.Inventory,
-    );
-
-    this.httpClient
-      .delete(
-        url,
-        this.settingsService.createRequestOptions({ ['announcementSequence']: announcementSequence }),
-      )
-      .pipe(take(1))
-      .subscribe(
-        (_) => {
-          this.eventDispatchService.dispatchEvent('inventoryAnnouncementDeleted', { id: announcementId });
-          this.refreshAnnouncements();
-        },
-        (error) => {
-          throw error;
-        },
-      );
+    this.eventDispatchService.dispatchEvent('inventoryAnnouncementDeleted', {
+      id: announcementId,
+      sequence: announcementSequence,
+    });
+    this.refreshAnnouncements();
   }
 
   ngOnDestroy(): void {
@@ -113,47 +88,27 @@ export class InventoryAnnouncementService implements OnDestroy {
   }
 
   refreshAvailableAnnouncements() {
-    const url = this.settingsService.createUrl(
-      `vehicles/v1/${this.inventoryId}/availableSpecialAnnouncements`,
-      ApiUrlType.Inventory,
+    of(this.availableJson).subscribe(
+      (announcements) => {
+        const translatedAndSortedAnnouncements = announcements;
+        this.availableAnnouncementOptions$.next(translatedAndSortedAnnouncements);
+      },
+      (error) => {
+        throw error;
+      },
     );
-
-    this.httpClient
-      .get<AnnouncementOption[]>(url, this.settingsService.createRequestOptions())
-      .pipe(take(1))
-      .subscribe(
-        (announcements) => {
-          const translatedAndSortedAnnouncements = this.translationService.translateAndSortAnnouncementsByDescription(
-            announcements,
-          );
-          this.availableAnnouncementOptions$.next(translatedAndSortedAnnouncements);
-        },
-        (error) => {
-          throw error;
-        },
-      );
   }
 
   refreshInventoryAnnouncements() {
-    const url = this.settingsService.createUrl(
-      `vehicles/v1/${this.inventoryId}/specialAnnouncements`,
-      ApiUrlType.Inventory,
+    return of(this.sampleJson).subscribe(
+      (announcements) => {
+        const sortedAnnouncements = announcements;
+        this.eventDispatchService.dispatchEvent('inventoryAnnouncementsUpdated', sortedAnnouncements);
+        this.inventoryAnnouncementsList$.next(sortedAnnouncements);
+      },
+      (error) => {
+        throw error;
+      },
     );
-
-    return this.httpClient
-      .get<InventoryAnnouncement[]>(url, this.settingsService.createRequestOptions())
-      .pipe(take(1))
-      .subscribe(
-        (announcements) => {
-          const sortedAnnouncements = this.translationService.translateAndSortAnnouncementsByDescription(
-            announcements,
-          );
-          this.eventDispatchService.dispatchEvent('inventoryAnnouncementsUpdated', sortedAnnouncements);
-          this.inventoryAnnouncementsList$.next(sortedAnnouncements);
-        },
-        (error) => {
-          throw error;
-        },
-      );
   }
 }
